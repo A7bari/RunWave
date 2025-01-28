eebf7fc091edd11f7bd47cb3de66d8ac0ac98936
a7bari <mohammed.ahbari@usmba.ac.ma>
2025-01-28 23:51:03 +0100

    chore: Update code-execution-service with Postgres configuration options

--- code-execution-service/go.mod
+++ code-execution-service/go.mod
@@ -21,6 +21,12 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.23.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.5.5 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
@@ -30,6 +36,8 @@ require (
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	golang.org/x/arch v0.8.0 // indirect
 	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	gorm.io/gorm v1.25.10 // indirect
 )
 
 require (
@@ -72,6 +80,7 @@ require (
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/driver/postgres v1.5.11
 	k8s.io/api v0.31.0
 	k8s.io/apimachinery v0.31.0
 	k8s.io/klog/v2 v2.130.1 // indirect
--- code-execution-service/main.go
+++ code-execution-service/main.go
@@ -2,16 +2,15 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"path/filepath"
 
 	"github.com/A7bari/RunWave/internal/app"
 	coderunner "github.com/A7bari/RunWave/internal/code_runner"
 	"github.com/A7bari/RunWave/internal/config"
 	"github.com/A7bari/RunWave/internal/db"
+	"github.com/A7bari/RunWave/internal/services"
 	"github.com/A7bari/RunWave/internal/store"
 	"github.com/A7bari/RunWave/internal/taskqueue"
-	"github.com/A7bari/RunWave/internal/types"
 
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
@@ -23,12 +22,9 @@ var (
 	// Version is the version of the service
 	Version = "v0.0.1"
 
-	// taskQueues is a map of task queues for each language
-	taskQueues = make(map[string]taskqueue.TaskQueue)
-
 	// Distributed task queue
-	dQueuePub taskqueue.DqueuePublisher
-	dQueueRec taskqueue.DqueueReceiver
+	QueuePublisher taskqueue.QueuePublisher
+	QueueConsumer  taskqueue.QueueConsumer
 
 	// schedulers is a map of schedulers for each language
 	schedulers = make(map[string]*coderunner.Scheduler)
@@ -45,85 +41,80 @@ var (
 	// mStore is the main store
 	mStore store.Store
 
+	// taskService is the task service
+	taskService services.ITaskService
+
 	// podManager is the pod manager
 	podManager coderunner.PodManager
 )
 
 func main() {
-	home := homedir.HomeDir()
-	kubeconfig := filepath.Join(home, ".kube", "config")
-	var err error
-	// Build the config from the kubeconfig file
-	restConfig, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
-	if err != nil {
-		log.Fatalf("Error building kubeconfig: %v", err)
-	}
 
-	clientset, err = kubernetes.NewForConfig(restConfig)
+	// setup k8s
+	restConfig, clientset, err := setupK8s()
 	if err != nil {
-		log.Fatalf("Error creating Kubernetes client: %v", err)
+		panic(fmt.Sprintf("Error setting up k8s: %v", err))
 	}
 
 	serviceConf := config.GetConfig()
 
 	// create the main store
 	mStore = db.GetPostgresStore()
-	
+	taskService = services.NewTaskService(mStore)
 
 	rbmq, err := db.GetRabbitMQConn()
 	if err != nil {
-		log.Fatalf("Error getting RabbitMQ connection: %v", err)
+		panic(fmt.Sprintf("Error connecting to RabbitMQ: %v", err))
 	}
 
-	dQueuePub = rbmq
-	dQueueRec = rbmq
+	QueuePublisher = rbmq
+	QueueConsumer = rbmq
 
 	// create the task qu
 	// create the pod manager
 	podManager = coderunner.NewPodManager(clientset, restConfig, serviceConf.Namespace)
 
-	// create task queues and schedulers for each language
-	for _, lang := range serviceConf.Languages {
-		q := db.GetInMemTaskQueue(300)
-		taskQueues[lang] = q
-
-		// create a scheduler
-		s := coderunner.NewScheduler(coderunner.SchedulerOpts{
-			TaskQueue:  q,
-			PodManager: podManager,
-			OnEndExec: func(task taskqueue.Task) {
-				output, isError := task.GetResult()
-				fmt.Printf("Scheduler: Task %s ended with output: %s\n", task.GetTaskID(), output)
-				err := mStore.SaveResult(types.TaskOutput{
-					TaskID:  task.GetTaskID(),
-					Output:  output,
-					Status:  task.GetStatus(),
-					IsError: isError,
-					Lang:    task.GetLanguage(),
-					Version: task.GetRetryCnt(),
-				})
-				if err != nil {
-					fmt.Printf("Scheduler [Error saving task] :result: %s\n", err)
-				}
+	// create a scheduler
+	s := coderunner.NewScheduler(
+		coderunner.SchedulerOpts{
+			Consumer:    QueueConsumer,
+			TaskService: taskService,
+			PodManager:  podManager,
+			OnEndExec: func(lang, taskId string) {
+				fmt.Printf("Scheduler: Task %s in language: %s\n", taskId, lang)
 			},
 
-			OnFailExec: func(task taskqueue.Task) {
-				fmt.Printf("Scheduler [failed task]: Task %s failed with err: %s\n", task.GetTaskID(), task.GetError())
-				err := mStore.UpdateTask(task)
-				if err != nil {
-					fmt.Printf("Scheduler [Error updating task]: %s\n", err)
-				}
+			OnFailExec: func(lang, taskId string) {
+				fmt.Printf("Scheduler [failed task]: Task %s \n", taskId)
 			},
-		})
+		},
+		config.GetConfig().Languages...,
+	)
 
-		schedulers[lang] = s
-
-		go s.StartWorkers()
-	}
+	go s.StartWorkers()
 
 	// start the server
 	server = app.NewServer(app.ServerOpts{
-		Queues: taskQueues,
+		TaskService:   taskService,
+		TaskPublisher: QueuePublisher,
 	})
 	server.Start()
+}
+
+func setupK8s() (*rest.Config, *kubernetes.Clientset, error) {
+	home := homedir.HomeDir()
+	kubeconfig := filepath.Join(home, ".kube", "config")
+
+	// Build the config from the kubeconfig file
+	restConfig, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		return nil, nil, fmt.Errorf("Error building kubeconfig: %w", err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(restConfig)
+	if err != nil {
+		return nil, nil, fmt.Errorf("Error creating Kubernetes client: %w", err)
+	}
+
+	return restConfig, clientset, nil
 }
